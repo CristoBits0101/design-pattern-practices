56afa5d09c7d349fd998d9446148aa3fe4f22045
Cristo Suárez <cristobits0101@gmail.com>
2025-05-11 12:28:09 +0100

    Refactor PhoneDecorator class to improve code structure and clarity

--- src/main/java/com/patterns/structural/decorator/decorators/PhoneDecorator.java
+++ src/main/java/com/patterns/structural/decorator/decorators/PhoneDecorator.java
@@ -2,12 +2,34 @@ package com.patterns.structural.decorator.decorators;
 
 import com.patterns.structural.decorator.services.PhoneService;
 
-// Step 4: Create a class decorator that implements the PhoneService interface
+// Step 4.1: Create a class decorator that implements the PhoneService interface
 public class PhoneDecorator implements PhoneService {
 
-    // Step 4.1: Create a PhoneService object
+    /**
+     * Attributes
+     */
+
+    // Step 4.2: Create a PhoneService object
+    private PhoneService phoneService;
+
+    /**
+     * Constructors
+     */
+
+    // Step 4.3: Create a constructor that takes the interface as a parameter
+    PhoneDecorator(PhoneService phoneService) {
+        // Step 4.4: Modify the interface to use the PhoneService object
+        this.phoneService = phoneService;
+    }
+
+    /**
+     * Methods
+     */
+
+    // Step 4.5: Create a PhoneService object
     @Override
     public void create() {
+        this.phoneService.create();
     }
 
 }
