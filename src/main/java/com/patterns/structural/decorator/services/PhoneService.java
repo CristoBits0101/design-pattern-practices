13636c2fe9ac0ee733b53ca1b80b9c1b895965a9
Cristo Suárez <cristobits0101@gmail.com>
2025-05-17 17:07:02 +0100

    Fix comment order in PhoneService for improved clarity

--- src/main/java/com/patterns/structural/decorator/services/PhoneService.java
+++ src/main/java/com/patterns/structural/decorator/services/PhoneService.java
@@ -1,7 +1,8 @@
 package com.patterns.structural.decorator.services;
 
-// Step 1: Create a interface with a method to be implemented
 // Let's imitate an old code!!
+
+// Step 1: Create a interface with a method to be implemented
 public interface PhoneService {
 
     void create();
