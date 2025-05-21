a0f1ffa153d59ee43e772fefb23369598c46764f
Cristo RPS <cristobits0101@gmail.com>
2025-05-22 00:15:35 +0100

    Finish pattern strategy

--- src/main/java/com/patterns/behavior/strategy/Main.java
+++ src/main/java/com/patterns/behavior/strategy/Main.java
@@ -1,17 +1,42 @@
 package com.patterns.behavior.strategy;
 
+import java.util.ArrayList;
+
+import com.patterns.behavior.strategy.services.Usuarios;
+import com.patterns.behavior.strategy.services.UsuariosFicheroImpl;
 import com.patterns.behavior.strategy.services.UsuariosMemoriaImpl;
 
 public class Main {
+
     public static void main(String[] args) {
-        UsuariosMemoriaImpl usuariosMemoria = new UsuariosMemoriaImpl();
 
-        usuariosMemoria.crear("Juan");
-        usuariosMemoria.crear("Maria");
-        usuariosMemoria.crear("Pedro");
+        Usuarios usuarios;
+        boolean memoria = true;
 
-        for (String usuario : usuariosMemoria.listar()) {
+        usuarios = memoria ? new UsuariosMemoriaImpl() : new UsuariosFicheroImpl();
+
+        crear(usuarios, "Juan");
+        crear(usuarios, "Maria");
+        crear(usuarios, "Pedro");
+
+        for (String usuario : usuarios.listar()) {
             System.out.println("Usuario: " + usuario);
         }
+        
     }
+
+    /**
+     * La interface recibe la instancia de la clase que la implementa
+     * 
+     * @param usuario
+     * @param nombre
+     */
+    public static void crear(Usuarios usuario, String nombre) {
+        usuario.crear(nombre);
+    }
+
+    public static ArrayList<String> listar(Usuarios usuarios) {
+        return usuarios.listar();
+    }
+
 }
